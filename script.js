fb3ec1ad008c494ef7e9c479589fbcb3205ce68e
Thirupathi Rao <162027483+thirupathi4@users.noreply.github.com>
2025-11-07 13:54:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function processArray(){
+	let output=document.getElementById("output");
+	new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4])
+		},3000)
+	})
+	.then((arr)=>{
+		const evens=arr.filter(num=>num%2===0);
+		setTimeout(()=>{
+			output.textContent=evens;
+		},1000);
+		return evens;
+	})
+	.then((evens)=>{
+		const double=evens.map(n=>n*2);
+		setTimeout(()=>{
+			output.textContent=double;
+		},2000);
+	});
+}
